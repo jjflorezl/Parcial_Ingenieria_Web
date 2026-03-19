f141234311bef58031a0bf21ae729974a20e1701
lazermortal <juanjoseflorezlopez@gmail.com>
2026-03-18 21:08:29 -0500

    primera compilacion de scalar

--- Parcial_Ingenieria_Web/Program.cs
+++ Parcial_Ingenieria_Web/Program.cs
@@ -5,6 +5,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 using System.Text;
+using Parcial_Ingenieria_Web.DAO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
