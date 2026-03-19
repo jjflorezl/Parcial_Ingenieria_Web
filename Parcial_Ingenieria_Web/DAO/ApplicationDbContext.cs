3ecd9b6400a1dc3c2f391712ddf2c49e6f78f69b
lazermortal <juanjoseflorezlopez@gmail.com>
2026-03-18 20:54:41 -0500

    creacion del auth

---
+++ Parcial_Ingenieria_Web/DAO/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Parcial_Ingenieria_Web.DAO
+{
+        public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+        {
+
+            //Constructor que recibe las opciones de conexión a la bd para tener contexto de esta
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        }
+    }
+
--- Parcial_Ingenieria_Web/Program.cs
+++ Parcial_Ingenieria_Web/Program.cs
@@ -1,6 +1,48 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Scalar.AspNetCore;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//Extraemos del archivo appsettings.json la cadena de conexión a la base de datos
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Le pasas las opciones de configuración al constructor de ApplicationDbContext que hereda las propiedades de DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => {
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+    };
+});
+
 // Add services to the container.
+// Aquí agregamos la inyección de dependencia de nuestros servicios
+// sin esto, los controladores no van a servir ya que no se inyecta el constructor o el objeto
+// que permite ir y llamar la capa lógica que tiene las operaciones CRUD o demás reglas de negocio
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -8,10 +50,13 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    // Agregamos la inicialización de Scalar (Documentación)
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
