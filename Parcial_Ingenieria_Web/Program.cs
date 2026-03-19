018776d071e0d046186453c9536252294bcc2461
lazermortal <juanjoseflorezlopez@gmail.com>
2026-03-19 08:17:44 -0500

    inicio

--- Parcial_Ingenieria_Web/Program.cs
+++ Parcial_Ingenieria_Web/Program.cs
@@ -18,36 +18,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 // Le pasas las opciones de configuración al constructor de ApplicationDbContext que hereda las propiedades de DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => {
-    options.Password.RequireDigit = true;
-    options.Password.RequiredLength = 8;
-    options.Password.RequireNonAlphanumeric = false;
-})
-.AddEntityFrameworkStores<ApplicationDbContext>()
-.AddDefaultTokenProviders();
 
-builder.Services.AddAuthentication(options => {
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options => {
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
-    };
-});
-
-// Add services to the container.
-// Aquí agregamos la inyección de dependencia de nuestros servicios
-// sin esto, los controladores no van a servir ya que no se inyecta el constructor o el objeto
-// que permite ir y llamar la capa lógica que tiene las operaciones CRUD o demás reglas de negocio
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
